90d19d6a61a3b3d846dc0c2c6b97e6ad420679f3
Attila Kerekes <ak@enrg8.com>
2022-12-16 19:40:05 +0100

    advent of code 2022 day 2

--- adventofcode2022/__tests__/day1.test.ts
+++ adventofcode2022/__tests__/day1.test.ts
@@ -1,9 +1,8 @@
 import * as fs from "fs";
 import * as path from "path";
-import { getElvWithMostCalories } from "../day1";
+import { getTop3Calories, getTopCalories } from "../day1";
 
-it("should return the elv number with most calories 1", () => {
-  const input = `1000
+const testInput = `1000
 2000
 3000
 
@@ -18,14 +17,40 @@ it("should return the elv number with most calories 1", () => {
 
 10000`;
 
-  const elvNumber = getElvWithMostCalories(input);
-});
+describe("Advent of Code", () => {
+  describe("Day 1", () => {
+    it("should return the elv number with most calories 1", () => {
+      const topCalories = getTopCalories(testInput);
 
-it("should return the elv number with most calories", () => {
-  const input = fs.readFileSync(path.resolve(__dirname, "../day1.txt"), {
-    encoding: "utf8",
-    flag: "r",
+      expect(topCalories).toBe(24000);
+    });
+
+    it("should return the elv number with most calories", () => {
+      const input = fs.readFileSync(path.resolve(__dirname, "../day1.txt"), {
+        encoding: "utf8",
+        flag: "r",
+      });
+
+      const topCalories = getTopCalories(input);
+
+      expect(topCalories).toBe(67658);
+    });
+
+    it("should return the elv number with most calories 1", () => {
+      const actual = getTop3Calories(testInput);
+
+      expect(actual).toBe(45000);
+    });
+
+    it("should return the elv number with most calories", () => {
+      const input = fs.readFileSync(path.resolve(__dirname, "../day1.txt"), {
+        encoding: "utf8",
+        flag: "r",
+      });
+
+      const actual = getTop3Calories(input);
+
+      expect(actual).toBe(200158);
+    });
   });
-
-  const elvNumber = getElvWithMostCalories(input);
 });
