faa1615268d28d57555d37388fed334faf90a04d
Attila Kerekes <ak@enrg8.com>
2022-12-18 19:32:14 +0100

    advent of code 2022 day 8

---
+++ 2022/__tests__/day8.test.ts
@@ -0,0 +1,48 @@
+import * as fs from "fs";
+import * as path from "path";
+import { getSmallestDirToDelete, getTotalSize } from "../day7";
+import { getBestScenicScore, getVisibleTreeCount } from "../day8";
+
+const testInput = `30373
+25512
+65332
+33549
+35390`;
+
+describe("Advent of Code", () => {
+  describe("Day 8", () => {
+    it("returns number of visible trees", () => {
+      const actual = getVisibleTreeCount(testInput);
+
+      expect(actual).toEqual(21);
+    });
+
+    it("returns number of visible trees from file", () => {
+      const input = fs.readFileSync(path.resolve(__dirname, "../day8.txt"), {
+        encoding: "utf8",
+        flag: "r",
+      });
+
+      const actual = getVisibleTreeCount(input);
+
+      expect(actual).toEqual(1870);
+    });
+
+    it("returns the best scenic score", () => {
+      const actual = getBestScenicScore(testInput);
+
+      expect(actual).toEqual(8);
+    });
+
+    it("returns the best scenic score from file", () => {
+      const input = fs.readFileSync(path.resolve(__dirname, "../day8.txt"), {
+        encoding: "utf8",
+        flag: "r",
+      });
+
+      const actual = getBestScenicScore(input);
+
+      expect(actual).toEqual(517440);
+    });
+  });
+});
