3c28bb61f7d51f72e8d8f86fadcd718552e1588c
Attila Kerekes <ak@enrg8.com>
2022-12-21 19:49:59 +0100

    advent of code 2022 day 14 part 1

---
+++ 2022/day14.ts
@@ -0,0 +1,135 @@
+import { execSync } from "child_process";
+import fs from "fs";
+import path from "path";
+
+export type World = Record<string, "#" | "o">;
+
+function getPosition(positionString: string): [number, number] {
+  return [
+    Number.parseInt(positionString.split(",")[0], 10),
+    Number.parseInt(positionString.split(",")[1], 10),
+  ];
+}
+
+function getSmaller(a: number, b: number) {
+  return a > b ? b : a;
+}
+
+function getBigger(a: number, b: number) {
+  return a > b ? a : b;
+}
+
+export const getWorld = (walls: string[]) => {
+  const world: World = {};
+
+  walls.forEach((wall) => {
+    const wallParts = wall.split(" -> ");
+
+    wallParts.forEach((wallPart, i) => {
+      if (!wallParts[i + 1]) {
+        return;
+      }
+
+      const startPosition = getPosition(wallPart);
+      const endPosition = getPosition(wallParts[i + 1]);
+
+      if (startPosition[0] === endPosition[0]) {
+        const start = getSmaller(startPosition[1], endPosition[1]);
+        const end = getBigger(endPosition[1], startPosition[1]);
+
+        for (let j = start; j <= end; j += 1) {
+          world[`${startPosition[0]},${j}`] = "#";
+        }
+        return;
+      }
+
+      const start = getSmaller(startPosition[0], endPosition[0]);
+      const end = getBigger(endPosition[0], startPosition[0]);
+
+      for (let j = start; j <= end; j += 1) {
+        world[`${j},${startPosition[1]}`] = "#";
+      }
+    });
+  });
+  return world;
+};
+
+function getArea(world: World) {
+  let startX = Number.MAX_SAFE_INTEGER;
+  let endX = 0;
+  const startY = 0;
+  let endY = 0;
+
+  Object.keys(world).forEach((position) => {
+    const [x, y] = getPosition(position);
+    if (x < startX) startX = x;
+    if (x > endX) endX = x;
+    if (y > endY) endY = y;
+  });
+
+  return { startX, endX, startY, endY };
+}
+
+export const getMap = (world: World) => {
+  const { startX, endX, endY } = getArea(world);
+
+  let map = "";
+
+  for (let y = 0; y <= endY; y += 1) {
+    map += `\n$`;
+    for (let x = startX; x <= endX; x += 1) {
+      if (world[`${x},${y}`]) {
+        map += world[`${x},${y}`];
+      } else {
+        map += " ";
+      }
+    }
+  }
+
+  return map;
+};
+
+function canMoveDown([x, y]: number[], world: World) {
+  const worldElement = world[`${x},${y + 1}`];
+  return !["#", "o"].includes(world[`${x},${y + 1}`]);
+}
+
+function canMoveLeft([x, y]: number[], world: World) {
+  return !["#", "o"].includes(world[`${x - 1},${y + 1}`]);
+}
+
+function canMoveRight([x, y]: number[], world: World) {
+  return !["#", "o"].includes(world[`${x + 1},${y + 1}`]);
+}
+
+export const getSand = (input) => {
+  const walls = input.split("\n");
+  const world = getWorld(walls);
+  const { endY } = getArea(world);
+
+  let fellOff = false;
+  let sandPosition = [500, 0];
+  let sandUnit = 0;
+
+  while (!fellOff) {
+    if (canMoveDown(sandPosition, world)) {
+      sandPosition = [sandPosition[0], sandPosition[1] + 1];
+    } else if (canMoveLeft(sandPosition, world)) {
+      sandPosition = [sandPosition[0] - 1, sandPosition[1] + 1];
+    } else if (canMoveRight(sandPosition, world)) {
+      sandPosition = [sandPosition[0] + 1, sandPosition[1] + 1];
+    } else {
+      world[`${sandPosition[0]},${sandPosition[1]}`] = "o";
+      sandPosition = [500, 0];
+      sandUnit += 1;
+    }
+
+    if (sandPosition[1] > endY) {
+      fellOff = true;
+    }
+  }
+
+  console.log(getMap(world));
+
+  return sandUnit;
+};
