3fd8ebb2af5cdcc2cceb4069a86278e85c057da5
Attila Kerekes <ak@enrg8.com>
2022-12-22 01:52:09 +0100

    advent of code 2022 day 15

---
+++ 2022/__tests__/day15.test.ts
@@ -0,0 +1,70 @@
+import * as fs from "fs";
+import * as path from "path";
+import { getBeaconPosition, getCoveredPositionsAtLine } from "../day15";
+
+const input = `Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3`;
+
+describe("Advent of Code", () => {
+  describe("Day 15", () => {
+    it("returns number of positions where a beacon cannot be present single", () => {
+      const actual = getCoveredPositionsAtLine(
+        "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+        10
+      );
+
+      expect(actual).toEqual(12);
+    });
+    it("returns number of positions where a beacon cannot be present", () => {
+      const actual = getCoveredPositionsAtLine(input, 10);
+
+      expect(actual).toEqual(26);
+    });
+
+    it("returns number of positions where a beacon cannot be present - file input", () => {
+      const fileInput = fs.readFileSync(
+        path.resolve(__dirname, "../day15.txt"),
+        {
+          encoding: "utf8",
+          flag: "r",
+        }
+      );
+
+      const actual = getCoveredPositionsAtLine(fileInput, 2000000);
+
+      expect(actual).toEqual(5073496);
+    });
+
+    it("returns position of the beacon", () => {
+      const actual = getBeaconPosition(input, [0, 20]);
+
+      expect(actual).toEqual(56000011);
+    });
+
+    it("returns position of the beacon - file input", () => {
+      const fileInput = fs.readFileSync(
+        path.resolve(__dirname, "../day15.txt"),
+        {
+          encoding: "utf8",
+          flag: "r",
+        }
+      );
+
+      const actual = getBeaconPosition(fileInput, [0, 4000000]);
+
+      expect(actual).toEqual(13081194638237);
+    });
+  });
+});
---
+++ 2022/day15.ts
@@ -0,0 +1,204 @@
+/* eslint-disable no-continue */
+type Position = [number, number];
+
+class SensorBeaconPair {
+  public readonly distance: number;
+
+  constructor(
+    public readonly sx: number,
+    public readonly sy: number,
+    public readonly bx: number,
+    public readonly by: number
+  ) {
+    this.distance = this.getDistance(bx, by);
+  }
+
+  public getDistance = (x, y) => Math.abs(x - this.sx) + Math.abs(y - this.sy);
+
+  public static createFromDescription(description): SensorBeaconPair {
+    const [a, sx, sy, bx, by] = description.match(
+      /^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$/
+    );
+
+    return new SensorBeaconPair(
+      Number.parseInt(sx, 10),
+      Number.parseInt(sy, 10),
+      Number.parseInt(bx, 10),
+      Number.parseInt(by, 10)
+    );
+  }
+}
+
+function getCoveredPointsAtLine(pairs: SensorBeaconPair[], line): Set<number> {
+  const yCovered = new Set<number>();
+
+  for (let j = 0; j < pairs.length; j += 1) {
+    const pair = pairs[j];
+    const { sx, sy, distance } = pair;
+    if (sy - distance <= line && sy + distance >= line) {
+      const xDistance = Math.abs(sy - line);
+      const yXStart = sx - distance + xDistance;
+      const yXEnd = sx + distance - xDistance;
+
+      for (let i = yXStart; i <= yXEnd; i += 1) {
+        yCovered.add(i);
+      }
+    }
+  }
+
+  return yCovered;
+}
+
+function isPositionCovered(pairs: SensorBeaconPair[], position: Position) {
+  const [x, y] = position;
+  let isCovered = false;
+
+  pairs.find((pair) => {
+    const { distance } = pair;
+    const myDistance = pair.getDistance(x, y);
+
+    if (myDistance <= distance) {
+      isCovered = true;
+      return true;
+    }
+
+    return false;
+  });
+
+  return isCovered;
+}
+
+export const getCoveredPositionsAtLine = (input, line) => {
+  const reports = input.split("\n");
+
+  const pairs = reports.map(SensorBeaconPair.createFromDescription);
+
+  const lineCovered = getCoveredPointsAtLine(pairs, line);
+
+  return lineCovered.size - 1;
+};
+
+function findUncoveredPosition(
+  start: number,
+  end: number,
+  pairs: SensorBeaconPair[]
+): Position {
+  let result: Position = [0, 0];
+
+  pairs.find(({ sx, sy, distance }) => {
+    const quarters = {
+      q1: {
+        x: {
+          start: sx,
+          end: sx + distance + 1,
+        },
+        y: {
+          start: sy - distance - 1,
+          end: sy,
+        },
+      },
+      q2: {
+        x: {
+          start: sx + distance + 1,
+          end: sx,
+        },
+        y: {
+          start: sy,
+          end: sy + distance + 1,
+        },
+      },
+      q3: {
+        x: {
+          start: sx,
+          end: sx - distance - 1,
+        },
+        y: {
+          start: sy + distance + 1,
+          end: sy,
+        },
+      },
+      q4: {
+        x: {
+          start: sx - distance - 1,
+          end: sx,
+        },
+        y: {
+          start: sy,
+          end: sy - distance - 1,
+        },
+      },
+    };
+
+    for (
+      let x = quarters.q1.x.start, y = quarters.q1.y.start;
+      x <= quarters.q1.x.end;
+      x += 1, y += 1
+    ) {
+      if (x > end || x < start || y > end || y < start) {
+        continue;
+      }
+      if (!isPositionCovered(pairs, [x, y])) {
+        result = [x, y];
+        return true;
+      }
+    }
+
+    for (
+      let x = quarters.q2.x.start, y = quarters.q2.y.start;
+      x <= quarters.q2.x.end;
+      x += 1, y -= 1
+    ) {
+      if (x > end || x < start || y > end || y < start) {
+        continue;
+      }
+      if (!isPositionCovered(pairs, [x, y])) {
+        result = [x, y];
+        return true;
+      }
+    }
+
+    for (
+      let x = quarters.q3.x.start, y = quarters.q3.y.start;
+      x >= quarters.q3.x.end;
+      x -= 1, y -= 1
+    ) {
+      if (x > end || x < start || y > end || y < start) {
+        continue;
+      }
+      if (!isPositionCovered(pairs, [x, y])) {
+        result = [x, y];
+        return true;
+      }
+    }
+
+    for (
+      let x = quarters.q4.x.start, y = quarters.q3.y.start;
+      x <= quarters.q4.x.end;
+      x += 1, y -= 1
+    ) {
+      if (x > end || x < start || y > end || y < start) {
+        continue;
+      }
+      if (!isPositionCovered(pairs, [x, y])) {
+        result = [x, y];
+        return true;
+      }
+    }
+
+    return false;
+  });
+
+  return result;
+}
+
+export const getBeaconPosition = (input, [start, end]) => {
+  const reports = input.split("\n");
+
+  const pairs: SensorBeaconPair[] = reports.map(
+    SensorBeaconPair.createFromDescription
+  );
+
+  const [x, y] = findUncoveredPosition(start, end, pairs);
+
+  return x * 4000000 + y;
+};
