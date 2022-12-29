056136368064aa47e4cdba433d1956bfb3d40456
Attila Kerekes <ak@enrg8.com>
2022-12-29 18:00:04 +0100

    add day generator

---
+++ day.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+year=$1;
+day=$2;
+
+
+if [ ! -d "$year" ];
+then
+  mkdir "$year"
+fi
+
+if [ ! -d "$year/__tests__" ];
+then
+  mkdir "$year/__tests__"
+fi
+
+touch "$year/day$day.ts";
+touch "$year/day$day.txt";
+touch "$year/__tests__/day$day.test.ts";
+
+cat template.day.ts | sed s/'$day'/$day/g > $year/day$day.ts
+cat template.test.ts | sed s/'$day'/$day/g > $year/__tests__/day$day.test.ts
