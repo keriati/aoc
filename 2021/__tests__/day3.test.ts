4bf36da55c8e4d687ab7fe39b6f23f0dad5a939a
Attila Kerekes <ak@enrg8.com>
2022-12-29 18:29:16 +0100

    add 2021 day 3 part 1

---
+++ 2021/__tests__/day3.test.ts
@@ -0,0 +1,36 @@
+import * as fs from "fs";
+import * as path from "path";
+import { getResult } from "../day3";
+
+describe("Advent of Code 2021", () => {
+  describe("Day 3: Binary Diagnostic", () => {
+    it("returns gamma rate * epsilon rate", () => {
+      const input = `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`;
+      const actual = getResult(input);
+
+      expect(actual).toBe(198);
+    });
+
+    it("returns gamma rate * epsilon rate - file input", () => {
+      const input = fs.readFileSync(path.resolve(__dirname, "../day3.txt"), {
+        encoding: "utf8",
+        flag: "r",
+      });
+
+      const actual = getResult(input);
+
+      expect(actual).toBe(3148794);
+    });
+  });
+});
