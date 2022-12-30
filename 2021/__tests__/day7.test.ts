631f1efe48e68c326c639f6c12a31b11d0922af1
Attila Kerekes <ak@enrg8.com>
2022-12-30 21:42:34 +0100

    add 2021 day 7

---
+++ 2021/__tests__/day7.test.ts
@@ -0,0 +1,43 @@
+import * as fs from "fs";
+import * as path from "path";
+import { getResult } from "../day7";
+
+describe("Advent of Code 2021", () => {
+  describe("Day 7: The Treachery of Whales", () => {
+    it("returns the result", () => {
+      const input = `16,1,2,0,4,2,7,1,2,14`;
+      const actual = getResult(input);
+
+      expect(actual).toBe(37);
+    });
+
+    it("returns the result - file input", () => {
+      const input = fs.readFileSync(path.resolve(__dirname, "../day7.txt"), {
+        encoding: "utf8",
+        flag: "r",
+      });
+
+      const actual = getResult(input);
+
+      expect(actual).toBe(343468);
+    });
+
+    it("returns the result part 2", () => {
+      const input = `16,1,2,0,4,2,7,1,2,14`;
+      const actual = getResult(input, false);
+
+      expect(actual).toBe(168);
+    });
+
+    it("returns the result part 2 - file input", () => {
+      const input = fs.readFileSync(path.resolve(__dirname, "../day7.txt"), {
+        encoding: "utf8",
+        flag: "r",
+      });
+
+      const actual = getResult(input, false);
+
+      expect(actual).toBe(96086265);
+    });
+  });
+});
---
+++ 2021/day7.ts
@@ -0,0 +1,33 @@
+/* eslint-disable */
+export const getResult = (input, constantRate = true) => {
+  const crabPositionsString = input.split(",");
+
+  const crabPositions = crabPositionsString.map((pos) =>
+    Number.parseInt(pos, 10)
+  );
+
+  const [min, max] = crabPositions.reduce(
+    ([min, max], pos) => {
+      min = pos < min ? pos : min;
+      max = pos > max ? pos : max;
+
+      return [min, max];
+    },
+    [0, 0]
+  );
+
+  let positionDistances = [];
+
+  for (let pos = min; pos <= max; pos += 1) {
+    let cost = 0;
+
+    crabPositions.forEach((crabPosition) => {
+      const d = Math.abs(pos - crabPosition);
+      cost += constantRate ? d : (d * (d + 1)) / 2;
+    });
+
+    positionDistances.push({ pos, cost });
+  }
+
+  return positionDistances.sort((a, b) => a.cost - b.cost)[0].cost;
+};
