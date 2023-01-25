d1a067a6121e3904e635ab4e57ba481c2fcbe774
Attila Kerekes <ak@enrg8.com>
2023-01-26 00:07:35 +0100

    add 2020 day 3

---
+++ 2020/__tests__/day03.test.ts
@@ -0,0 +1,53 @@
+import * as fs from "fs";
+import * as path from "path";
+import { getNumberOfTrees, getNumberOfTreesProd } from "../day03";
+
+const input = `..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#`;
+
+describe("Advent of Code 2020", () => {
+  describe("Day 03 Toboggan Trajectory", () => {
+    it("returns the number of trees on the slope", () => {
+      const actual = getNumberOfTrees(input);
+
+      expect(actual).toBe(7);
+    });
+
+    it("returns the number of trees on the slope - file input", () => {
+      const input = fs.readFileSync(path.resolve(__dirname, "../day03.txt"), {
+        encoding: "utf8",
+        flag: "r",
+      });
+
+      const actual = getNumberOfTrees(input);
+
+      expect(actual).toBe(254);
+    });
+
+    it("returns the result 2", () => {
+      const actual = getNumberOfTreesProd(input);
+
+      expect(actual).toBe(336);
+    });
+
+    it("returns the result 2 - file input", () => {
+      const input = fs.readFileSync(path.resolve(__dirname, "../day03.txt"), {
+        encoding: "utf8",
+        flag: "r",
+      });
+
+      const actual = getNumberOfTreesProd(input);
+
+      expect(actual).toBe(1666768320);
+    });
+  });
+});
