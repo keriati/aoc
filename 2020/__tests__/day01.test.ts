9c4fb968a86992ae58e1a22ad5d70d7f3e73e3c4
Attila Kerekes <ak@enrg8.com>
2023-01-26 22:06:37 +0100

    add 2020 day 4

--- 2020/__tests__/day01.test.ts
+++ 2020/__tests__/day01.test.ts
@@ -2,16 +2,16 @@ import * as fs from "fs";
 import * as path from "path";
 import { getResult, getResult2 } from "../day01";
 
-describe("Advent of Code 2020", () => {
-  describe("Day 01", () => {
-    it("returns the result", () => {
-      const input = `1721
+const input = `1721
 979
 366
 299
 675
-1456`
+1456`;
 
+describe("Advent of Code 2020", () => {
+  describe("Day 01 Report Repair", () => {
+    it("returns the result", () => {
       const actual = getResult(input);
 
       expect(actual).toBe(514579);
@@ -29,13 +29,6 @@ describe("Advent of Code 2020", () => {
     });
 
     it("returns the result part 2", () => {
-      const input = `1721
-979
-366
-299
-675
-1456`
-
       const actual = getResult2(input);
 
       expect(actual).toBe(241861950);
--- 2020/__tests__/day02.test.ts
+++ 2020/__tests__/day02.test.ts
@@ -1,39 +1,44 @@
 import * as fs from "fs";
 import * as path from "path";
-import { getResult, getResult2, isValid, isValid2 } from "../day02";
+import {
+  getValidPasswords,
+  getValidTobogganPasswords,
+  isValid,
+  isValidTobogan,
+} from "../day02";
 
 describe("Advent of Code 2020", () => {
-  describe("Day 02", () => {
-    it("returns the result", () => {
-      expect(isValid('abcde', '1-3 a')).toBe(true);
-      expect(isValid('cdefg', '1-3 b')).toBe(false);
-      expect(isValid('ccccccccc', '2-9 c')).toBe(true);
+  describe("Day 02 Password Philosophy", () => {
+    it("returns whether the password is valid or not", () => {
+      expect(isValid("abcde", "1-3 a")).toBe(true);
+      expect(isValid("cdefg", "1-3 b")).toBe(false);
+      expect(isValid("ccccccccc", "2-9 c")).toBe(true);
     });
 
-    it("returns the result - file input", () => {
+    it("returns the number of valid passwords - file input", () => {
       const input = fs.readFileSync(path.resolve(__dirname, "../day02.txt"), {
         encoding: "utf8",
         flag: "r",
       });
 
-      const actual = getResult(input);
+      const actual = getValidPasswords(input);
 
       expect(actual).toBe(398);
     });
 
-    it("returns the result part 2", () => {
-      expect(isValid2('abcde', '1-3 a')).toBe(true);
-      expect(isValid2('cdefg', '1-3 b')).toBe(false);
-      expect(isValid2('ccccccccc', '2-9 c')).toBe(false);
+    it("returns whether the password is valid or not - part 2", () => {
+      expect(isValidTobogan("abcde", "1-3 a")).toBe(true);
+      expect(isValidTobogan("cdefg", "1-3 b")).toBe(false);
+      expect(isValidTobogan("ccccccccc", "2-9 c")).toBe(false);
     });
 
-    it("returns the result part 2 - file input", () => {
+    it("returns the number of valid passwords - part 2 - file input", () => {
       const input = fs.readFileSync(path.resolve(__dirname, "../day02.txt"), {
         encoding: "utf8",
         flag: "r",
       });
 
-      const actual = getResult2(input);
+      const actual = getValidTobogganPasswords(input);
 
       expect(actual).toBe(562);
     });
