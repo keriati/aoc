00304e653b42603e8cd4b4a971681462b95f9750
Attila Kerekes <ak@enrg8.com>
2023-01-30 23:53:53 +0100

    add 2020 day 7 p2

--- 2020/__tests__/day07.test.ts
+++ 2020/__tests__/day07.test.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
-import { getParents } from "../day07";
+import { getChildren, getParents } from "../day07";
 
 const input = `light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
@@ -13,7 +13,7 @@ faded blue bags contain no other bags.
 dotted black bags contain no other bags.`;
 
 describe("Advent of Code 2020", () => {
-  describe("Day 07", () => {
+  describe("Day 07: Handy Haversacks", () => {
     it("returns the result", () => {
       const actual = getParents(input);
 
@@ -29,6 +29,36 @@ describe("Advent of Code 2020", () => {
       const actual = getParents(input);
 
       expect(actual).toBe(177);
+    });
+
+    it("returns the result 2", () => {
+      const actual = getChildren(input);
+
+      expect(actual).toBe(32);
+    });
+
+    it("returns the result 2", () => {
+      const input2 = `shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`;
+      const actual = getChildren(input2);
+
+      expect(actual).toBe(126);
+    });
+
+    it("returns the result - file input", () => {
+      const input = fs.readFileSync(path.resolve(__dirname, "../day07.txt"), {
+        encoding: "utf8",
+        flag: "r",
+      });
+
+      const actual = getChildren(input);
+
+      expect(actual).toBe(34988);
     });
   });
 });
--- 2020/day07.ts
+++ 2020/day07.ts
@@ -19,7 +19,7 @@ const getParentCount = (
   bag: string,
   map: Record<string, string[]>,
   visited: string[] = []
-) => {
+): number => {
   if (visited.indexOf(bag) !== -1) return 0;
 
   visited.push(bag);
@@ -38,4 +38,44 @@ export const getParents = (input) => {
   const map = getParentMap(lines);
   const parents = getParentCount("shiny gold", map);
   return parents - 1;
+};
+
+const getChildrenMap = (lines: string[]): Record<string, [string, number][]> =>
+  lines.reduce((res, l) => {
+    const [parent, children] = l.split(" bags contain ");
+
+    for (const child of children.split(", ")) {
+      if (child === "no other bags.") continue;
+
+      const [, amountS, childName] = child.match(/(\d+) ([a-z ]+) bag/);
+      const amount = parseInt(amountS, 10);
+
+      if (!(parent in res)) res[parent] = [];
+
+      res[parent].push([childName, amount]);
+    }
+
+    return res;
+  }, {});
+
+const getChildrenCount = (
+  bag: string,
+  map: Record<string, [string, number][]>
+): number => {
+  if (!(bag in map)) return 1;
+
+  let allBags = 1;
+
+  map[bag].forEach(([child, amount]) => {
+    allBags += amount * getChildrenCount(child, map);
+  });
+
+  return allBags;
+};
+
+export const getChildren = (input) => {
+  const lines = input.split("\n");
+  const map = getChildrenMap(lines);
+  const children = getChildrenCount("shiny gold", map);
+  return children - 1;
 };
