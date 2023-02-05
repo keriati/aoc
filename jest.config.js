3e0e47d425e8e7a5b64615bcb1b5becf23772dcd
Attila Kerekes <ak@enrg8.com>
2023-02-05 13:41:14 +0100

    update npm packages

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,4 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
   testPathIgnorePatterns: ["template.test.ts"],
-  globals: {
-    "ts-jest": {
-      isolatedModules: true,
-    },
-  },
 };
