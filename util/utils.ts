600cc69439558432adbb42fc87e31dc648925f90
Attila Kerekes <ak@enrg8.com>
2023-03-01 09:55:30 +0100

    add 2018 day 11

--- util/utils.ts
+++ util/utils.ts
@@ -12,3 +12,24 @@ export const defaultDict = (init) =>
 
 export const ABC_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export const ABC_LOWER = "abcdefghijklmnopqrstuvwxyz";
+
+const p1 = 311;
+const p2 = 997;
+
+/**
+ * Returns a unique integer (as key) from 3 integers
+ * @param a
+ * @param b
+ * @param c
+ */
+export const mk3n = (a: number, b: number, c: number): number =>
+  (a * p1 + b) * p2 + c;
+
+/**
+ * Returns a unique integer (as key) from 2 integers
+ * Uses Elegant Pairing
+ * @param a
+ * @param b
+ */
+export const mk2n = (a: number, b: number): number =>
+  a >= b ? a * a + a + b : a + b * b;
