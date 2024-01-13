4a0ed7eb845c275ffa088782c03abb6e7eedf24e
Attila Kerekes <ak@enrg8.com>
2024-01-13 20:47:11 +0100

    day17 refactored

--- 2023/day17.ts
+++ 2023/day17.ts
@@ -2,8 +2,8 @@ import { MinBucketQueue } from "bucket-priority-queue";
 
 const D = {
   N: 1,
-  S: 2,
-  E: 3,
+  E: 2,
+  S: 3,
   W: 4,
 } as const;
 
@@ -11,8 +11,7 @@ type Direction = (typeof D)[keyof typeof D];
 type Position = [number, number, Direction];
 type HeatLoss = number;
 type StepsInDirection = number;
-type Heuristic = number;
-type Step = [Heuristic, Position, HeatLoss, StepsInDirection];
+type Step = [...Position, HeatLoss, StepsInDirection];
 
 const getNextPositions: Record<
   Direction,
@@ -40,53 +39,56 @@ const getNextPositions: Record<
   ],
 };
 
-const cacheKey = ([, [x, y, direction], hl, steps]: Step): number =>
-  (y << 16) | (x << 8) | (direction << 4) | steps;
+const cacheKey = ([x, y, direction, , steps]: Step): number =>
+  // (y << (8 + 3 + 4)) | (x << (3 + 4)) | (direction << 4) | steps; // safer, slower
+  (((x + 1) * 43 + (y + 1) * 17) * 5 + direction) * 9 + steps; // speed 25ms, 45ms
 
-export const getLeastHeatLoss = (input: string) => {
-  const map = input.split("\n").map((line) => line.split("").map(Number));
+const findHeatLoss = (map: number[][], minSteps: number, maxSteps: number) => {
   const endX = map[0].length - 1;
   const endY = map.length - 1;
 
-  const startX = 0;
-  const startY = 0;
-
-  const startingStepEast: Step = [0, [startX, startY, D.E], 0, 0];
-  const startingStepSouth: Step = [0, [startX, startY, D.S], 0, 0];
+  const startingStepEast: Step = [0, 0, D.E, 0, 0];
+  const startingStepSouth: Step = [0, 0, D.S, 0, 0];
 
   const queue = new MinBucketQueue<Step>();
-  queue.push(startingStepEast, 0);
   queue.push(startingStepSouth, 0);
+  queue.push(startingStepEast, 0);
 
   const visited = new Set<number>();
+  visited.add(cacheKey(startingStepSouth));
+  visited.add(cacheKey(startingStepEast));
+
   // const visitedTiles = new Map<number, [number, number]>();
 
-  while (queue.size >= 0) {
-    const [, [x, y, direction], heatLoss, steps] = queue.pop();
+  while (queue.size) {
+    const [x, y, dir, heatLoss, steps] = queue.pop();
     // visitedTiles.set(mk2n(x, y), [x, y]);
+
     if (x === endX && y === endY) {
-      // break;
+      if (steps < minSteps) continue;
       return heatLoss;
     }
 
-    const nextPositions = getNextPositions[direction](x, y)
-      .filter(([, , newDirection]) =>
-        steps > 2 ? newDirection !== direction : true
-      )
-      .filter(([x, y]) => !(x < 0 || y < 0 || y > endY || x > endX));
+    const nextPositions = getNextPositions[dir](x, y);
 
-    for (const [nextX, nextY, nextDirection] of nextPositions) {
-      let nextStep: Step = [
-        heatLoss + map[nextY][nextX] + endX - nextX + endY - nextY,
-        [nextX, nextY, nextDirection],
-        heatLoss + map[nextY][nextX],
-        nextDirection === direction ? steps + 1 : 1,
+    for (const [nx, ny, nDir] of nextPositions) {
+      if (nx < 0 || ny < 0 || nx > endX || ny > endY) continue;
+      if (steps > maxSteps - 1 && dir === nDir) continue;
+      if (steps < minSteps && dir !== nDir) continue;
+
+      const next: Step = [
+        nx,
+        ny,
+        nDir,
+        heatLoss + map[ny][nx],
+        nDir === dir ? steps + 1 : 1,
       ];
+      const heuristic = heatLoss + map[ny][nx] + endX - nx + endY - ny;
 
-      const key = cacheKey(nextStep);
+      const key = cacheKey(next);
       if (!visited.has(key)) {
         visited.add(key);
-        queue.push(nextStep, nextStep[0]);
+        queue.push(next, heuristic);
       }
     }
   }
@@ -96,59 +98,12 @@ export const getLeastHeatLoss = (input: string) => {
   return -1;
 };
 
+export const getLeastHeatLoss = (input: string) => {
+  const map = input.split("\n").map((line) => line.split("").map(Number));
+  return findHeatLoss(map, 1, 3);
+};
+
 export const getLeastHeatLossUltra = (input: string) => {
   const map = input.split("\n").map((line) => line.split("").map(Number));
-  const endX = map[0].length - 1;
-  const endY = map.length - 1;
-
-  const startX = 0;
-  const startY = 0;
-
-  const startingStepEast: Step = [0, [startX, startY, D.E], 0, 0];
-  const startingStepSouth: Step = [0, [startX, startY, D.S], 0, 0];
-
-  const queue = new MinBucketQueue<Step>();
-  queue.push(startingStepEast, 0);
-  queue.push(startingStepSouth, 0);
-
-  const visited = new Set<number>();
-  // const visitedTiles = new Map<number, [number, number]>();
-
-  while (queue.size > 0) {
-    const [, [x, y, direction], heatLoss, steps] = queue.pop();
-    // visitedTiles.set(mk2n(x, y), [x, y]);
-
-    if (x === endX && y === endY) {
-      if (steps < 4) continue;
-      // break;
-      return heatLoss;
-    }
-
-    const nextPositions = getNextPositions[direction](x, y)
-      .filter(([, , newDirection]) => {
-        if (steps < 4) return newDirection === direction;
-        if (steps > 9) return newDirection !== direction;
-        return true;
-      })
-      .filter(([x, y]) => !(x < 0 || y < 0 || y > endY || x > endX));
-
-    for (const [nextX, nextY, newDirection] of nextPositions) {
-      let nextStep: Step = [
-        heatLoss + map[nextY][nextX] + endX - nextX + endY - nextY,
-        [nextX, nextY, newDirection],
-        heatLoss + map[nextY][nextX],
-        newDirection === direction ? steps + 1 : 1,
-      ];
-
-      const key = cacheKey(nextStep);
-      if (!visited.has(key)) {
-        visited.add(key);
-        queue.push(nextStep, nextStep[0]);
-      }
-    }
-  }
-
-  // render(map, visitedTiles);
-
-  return -1;
+  return findHeatLoss(map, 4, 10);
 };
