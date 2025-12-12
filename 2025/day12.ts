8e756730c410999ad83ddc870c2693eb5368dae3
Attila Kerekes <ak@enrg8.com>
2025-12-12 21:43:17 +0100

    update 2025 day 12

--- 2025/day12.ts
+++ 2025/day12.ts
@@ -1,17 +1,11 @@
 export const getResult = (input: string) =>
   input
-    .split("\n\n")
-    [input.split("\n\n").length - 1].split("\n")
+    .split("\n")
+    .slice(30)
     .map((line) =>
       line
         .match(/^(\d+)x(\d+): (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)$/)
         .map(Number)
     )
-    .map(([, w, h, ...amounts]) => [w * h, amounts] as [number, number[]])
-    .reduce(
-      (result, [area, shapeAmounts]) =>
-        shapeAmounts.reduce((t, amount, i) => t + 9 * amount, 0) <= area
-          ? result + 1
-          : result,
-      0
-    );
+    .map(([, m, e, rr, y, X, M, A, S]) => [m * e, rr + y + X + M + A + S])
+    .reduce((r, [a, s]) => (s * 9 <= a ? r + 1 : r), 0);
