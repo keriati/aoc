1575f7231cca71cd984c93d858f5b2621849aec7
Attila Kerekes <ak@enrg8.com>
2025-12-12 13:43:53 +0100

    update 2025 day 12

--- 2025/day09.ts
+++ 2025/day09.ts
@@ -29,10 +29,10 @@ export const getResultPart2 = (input: string) => {
 
   for (const [x1, y1] of points) {
     for (const [x2, y2] of points) {
-      const bx1 = Math.min(x1, x2);
-      const bx2 = Math.max(x1, x2);
-      const by1 = Math.min(y1, y2);
-      const by2 = Math.max(y1, y2);
+      const bxMin = Math.min(x1, x2);
+      const bxMax = Math.max(x1, x2);
+      const byMin = Math.min(y1, y2);
+      const byMax = Math.max(y1, y2);
 
       let intersects = false;
 
@@ -40,21 +40,19 @@ export const getResultPart2 = (input: string) => {
         const [px1, py1] = points[i];
         const [px2, py2] = points[(i + 1) % points.length];
 
-        if (
-          !(
-            Math.max(px1, px2) <= bx1 ||
-            bx2 <= Math.min(px1, px2) ||
-            Math.max(py1, py2) <= by1 ||
-            by2 <= Math.min(py1, py2)
-          )
-        ) {
+        const pxMax = Math.max(px1, px2);
+        const pxMin = Math.min(px1, px2);
+        const pyMax = Math.max(py1, py2);
+        const pyMin = Math.min(py1, py2);
+
+        if (pxMax > bxMin && bxMax > pxMin && pyMax > byMin && byMax > pyMin) {
           intersects = true;
           break;
         }
       }
 
       if (!intersects) {
-        const area = (bx2 - bx1 + 1) * (by2 - by1 + 1);
+        const area = (bxMax - bxMin + 1) * (byMax - byMin + 1);
         if (area > maxArea) {
           maxArea = area;
         }
